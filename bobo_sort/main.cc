3fdf370757de327f7fbec2a269bfc579ae129f1c
neoyxm <ynicle@gmail.com>
2012-06-07 06:26:44 +0000

    add the recursive version for bobo sort

--- bobo_sort/main.cc
+++ bobo_sort/main.cc
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+typedef int (*f_sort)(int *, int);
+
+void swap(int *pD1, int *pD2)
+{
+	int tmp = *pD1;
+	*pD1 =  *pD2;
+	*pD2 =  tmp;
+}
+
 int bobo_sort(int *pData, int nLen)
 {
 	if (!pData) return 1;	
@@ -10,11 +19,24 @@ int bobo_sort(int *pData, int nLen)
 		for (int j = 0; j < nLen - i - 1; j++)
 			if(pData[j] > pData[nLen - i - 1])
 			{
-				int  tmp = pData[j];
-				pData[j] = pData[nLen - i - 1];
-				pData[nLen - i - 1] = tmp;
+				swap(&pData[j], &pData[nLen - i - 1]);
 			}
 	
+	return 0;
+}
+
+int bobo_recusive_sort(int *pData, int nLen)
+{
+	if (!pData || nLen <= 1) return 0;
+
+	for (int i = 0; i < nLen - 1; i ++)
+	{
+		if (pData[i] > pData[nLen - 1])	
+			swap(&pData[i], &pData[nLen - 1]);
+	}
+
+	bobo_recusive_sort(pData, nLen - 1);
+
 	return 0;
 }
 
@@ -26,25 +48,42 @@ void print_data(int *pData, int nLen)
 		cout << pData[i]  << " ";
 }
 
+int test_framework(f_sort pSortFunc, int *pData, int nLen)
+{
+	cout << "Original data order:" ;
+	print_data(pData, nLen);
+	cout << endl;
+
+	if (pSortFunc(pData, nLen) != 0)
+	{
+		cerr << "Sort failed" << endl;
+		return 1;
+	}
+
+	cout << "Sorted data order:" ;
+	print_data(pData, nLen);
+	cout << endl;
+
+	return 0;
+}
+
 int main()
 {
 	int data[] = {10,2,7,9,20,15,99,50,32};	
-//	int data[] ={10};
+	int data2[] = {10,2,7,9,20,15,99,50,32};	
+	//int data[] ={10};
 	//int data[] ={};
+	//int  *data = NULL;
 	int length = sizeof(data)/sizeof(int);
 
-	
-	cout << "Original data order:" ;
-	print_data(data, length);
+	cout << "Loop version for Bobo sort:" << endl;
+	test_framework(bobo_sort, data, length);
+
 	cout << endl;
-
-	bobo_sort(data, length);
 	
-	cout << "Sorted data order:" ;
-	print_data(data, length);
-	cout << endl;
-
-
+	length = sizeof(data2)/sizeof(int);
+	cout << "Recursive version for Bobo sort:" << endl;
+	test_framework(bobo_recusive_sort, data2, length);
 
 	return 0;
 }
