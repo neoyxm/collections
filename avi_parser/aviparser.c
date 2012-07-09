14dec20aceeb1abc7996ee5a0649741a05ddd543
neoyxm <ynicle@gmail.com>
2012-07-09 05:39:45 +0000

    moved the macro and structure define to aviparser.h

--- avi_parser/aviparser.c
+++ avi_parser/aviparser.c
@@ -2,45 +2,8 @@
 #include <stdlib.h>
 #include <memory.h>
 
-#define mmioFOURCC(ch0, ch1, ch2, ch3)\
-    ((unsigned int)(unsigned char)(ch0) | ((unsigned int)(unsigned char)(ch1) << 8) |\
-    ((unsigned int)(unsigned char)(ch2) << 16) | ((unsigned int)(unsigned char)(ch3) << 24 ))
+#include "aviparser.h"
 
-#define mmioSIZE(ch0, ch1, ch2, ch3)\
-    ((int)(unsigned char)(ch0) | ((int)(unsigned char)(ch1) << 8) |\
-    ((int)(unsigned char)(ch2) << 16) | ((int)(unsigned char)(ch3) << 24 ))
-
-#define FOURCC_RIFF mmioFOURCC('R','I','F','F')
-#define FOURCC_LIST mmioFOURCC('L','I','S','T')
-#define FOURCC_AVI  mmioFOURCC('A','V','I',' ')
-#define FOURCC_hdrl  mmioFOURCC('h','d','r','l')
-#define FOURCC_avih  mmioFOURCC('a','v','i','h')
-#define FOURCC_strl  mmioFOURCC('s','t','r','l')
-#define FOURCC_strh  mmioFOURCC('s','t','r','h')
-#define FOURCC_strf  mmioFOURCC('s','t','r','f')
-#define FOURCC_strd  mmioFOURCC('s','t','r','d')
-#define FOURCC_divx  mmioFOURCC('d','i','v','x')
-#define FOURCC_DIVX  mmioFOURCC('D','I','V','X')
-#define FOURCC_vids  mmioFOURCC('v','i','d','s')
-
-typedef struct avitag avitag_t;
-typedef struct videoinfo videoinfo_t;
-struct avitag
-{
-    unsigned int fcc;
-    int size;
-    unsigned int flag;
-    int nested;
-    int has_sub;
-}; 
-
-struct videoinfo
-{
-	unsigned int tag_fcc;
-	unsigned int codec_fcc;
-	int has_strd;
-};
- 
 int parse_strh(FILE *fp, const avitag_t *tag, videoinfo_t * videoinfo)
 {
     unsigned char strh[56];
