2bd8a3c8585522c03eb1a9bba24c3e2f7def4e45
neoyxm <ynicle@gmail.com>
2016-06-27 22:06:46 +0800

    reformat the coding style with astyle

--- binary_search/main.cpp
+++ binary_search/main.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 const int data[] = {12, 14, 20, 23, 25,
-                      26, 31, 32, 36, 39,
-                      40, 43, 46, 48, 51
-};
+                    26, 31, 32, 36, 39,
+                    40, 43, 46, 48, 51
+                   };
 
 void print_usage(int argc, char **argv)
 {
     if (argc < 2)
     {
-       cout << "Usage: " << argv[0] << " target_num." << endl;
-       exit(1);
+        cout << "Usage: " << argv[0] << " target_num." << endl;
+        exit(1);
     }
 }
 
@@ -23,7 +23,7 @@ int binary_search (int start_pos, int end_pos, int target)
     {
         if (data[start_pos] == target)
             return start_pos;
-        else 
+        else
             return -1;
     } else {
         int half_pos =  (start_pos + end_pos) >> 1;
@@ -34,7 +34,7 @@ int binary_search (int start_pos, int end_pos, int target)
             return binary_search(half_pos + 1, end_pos, target);
         else if (data[half_pos] > target && half_pos > start_pos)
             return binary_search(start_pos, half_pos -1, target);
-        else 
+        else
             return -1;
     }
 
@@ -43,64 +43,64 @@ int binary_search (int start_pos, int end_pos, int target)
 
 int binary_search_non_recursive(int start_pos, int end_pos, int target)
 {
-	int target_pos  = -1;
+    int target_pos  = -1;
     int s = start_pos, e = end_pos;
 
     while ( s <= e)
-	{
-	    int half_pos  =  (s + e) >> 1;
+    {
+        int half_pos  =  (s + e) >> 1;
 
-		if (data[half_pos] == target) 
-		{
-			target_pos = half_pos;
-		    break;
-		} 
-		else if (data[half_pos] < target && e > half_pos)
-		{
+        if (data[half_pos] == target)
+        {
+            target_pos = half_pos;
+            break;
+        }
+        else if (data[half_pos] < target && e > half_pos)
+        {
             s = half_pos + 1;
-		    continue;
-		}
-		else if (data[half_pos] > target && half_pos > s)
-		{
+            continue;
+        }
+        else if (data[half_pos] > target && half_pos > s)
+        {
             e = half_pos - 1;
-		    continue;		
-		}
-		else 
-		    break;
-	}
+            continue;
+        }
+        else
+            break;
+    }
 
     return target_pos;
 }
 
 int main(int argc, char **argv)
 {
-	
-	int target = 0;
 
-	for (int i = 0; i < sizeof(data)/sizeof(int); i++)
-	{
-		cout << data[i] << " ";		
-	}
-	cout << endl;
+    int target = 0;
 
-
-	cout << "Please input you want:";
-    cin >> target;
-	cout << endl;
-
-	cout << "Recursive: The target: " << target;
-    cout << " at index: "  << binary_search(0, sizeof(data)/sizeof(int) - 1, target); 
+    for (int i = 0; i < sizeof(data)/sizeof(int); i++)
+    {
+        cout << data[i] << " ";
+    }
     cout << endl;
 
-	cout << "Non-Recursive: The target: " << target;
-    cout << " at index: "  << binary_search_non_recursive(0, sizeof(data)/sizeof(int) - 1, target); 
+
+    cout << "Please input you want:";
+    cin >> target;
+    cout << endl;
+
+    cout << "Recursive: The target: " << target;
+    cout << " at index: "  << binary_search(0, sizeof(data)/sizeof(int) - 1, target);
+    cout << endl;
+
+    cout << "Non-Recursive: The target: " << target;
+    cout << " at index: "  << binary_search_non_recursive(0, sizeof(data)/sizeof(int) - 1, target);
     cout << endl;
 
 #ifdef __WIN32__
-{
-    char c = 0;
-	cin >> c;
-}
+    {
+        char c = 0;
+        cin >> c;
+    }
 #endif
 
     return 0;
