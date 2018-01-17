a91234c7101a1664dd9b6a5eec0ba43a2b19b61f
Neoyxm <ynicle@gmail.com>
2018-01-17 13:31:26 +0800

    implemented the pre/center/after order browsing the tree

--- tree_browsing/main.cpp
+++ tree_browsing/main.cpp
@@ -20,6 +20,23 @@ void preOrderBrowse(PTNode pNode)
     preOrderBrowse(pNode->r);
 }
 
+void centerOrderBrowse(PTNode pNode)
+{
+    if (!pNode)
+        return;
+    centerOrderBrowse(pNode->l);
+    cout << pNode->data << endl;
+    centerOrderBrowse(pNode->r);
+}
+
+void afterOrderBrowse(PTNode pNode)
+{
+    if (!pNode)
+        return;
+    afterOrderBrowse(pNode->l);
+    afterOrderBrowse(pNode->r);
+    cout << pNode->data << endl;
+}
 int main()
 {
     TNode g = { 'g', NULL, NULL };
@@ -31,8 +48,13 @@ int main()
     TNode a = {'a', &b, &c};
     
     Tree p = &a;
-
+    
+    cout << "PreOrder:"<< endl;
     preOrderBrowse(p);
+    cout << "CenterOrder:"<< endl;
+    centerOrderBrowse(p);
+    cout << "AfterOrder:"<< endl;
+    afterOrderBrowse(p);
     
     int i;
     cin >> i;
